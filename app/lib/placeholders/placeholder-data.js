ee76905bbaecdfcd6189b5d09b1d689813ee3b1d
est091 <ellis0078@gmail.com>
2024-05-16 13:45:05 +0530

    Minor Changes

--- app/lib/placeholders/placeholder-data.js
+++ app/lib/placeholders/placeholder-data.js
@@ -2,8 +2,8 @@
 // https://nextjs.org/learn/dashboard-app/fetching-data
 const users = [
 	{ id: '410544b2-4001-4271-9855-fec4b6a6442a', name: 'User', email: 'admin@admin.com', password: 'Asdf@12345' },
-	{ id: '410544b2-4001-4271-9855-fec4b6a6442b', name: 'User', email: 'ellis.tarmaster@inventyv.com', password: 'Elli0078910$' },
-	{ id: '410544b2-4001-4271-9855-fec4b6a6442c', name: 'User', email: 'aman.verasia@inventyv.com', password: 'Aman@1234' },
+	{ id: '3958dc9e-712f-4377-85e9-fec4b6a6442a', name: 'User', email: 'ellis.tarmaster@inventyv.com', password: 'Elli0078910$' },
+	{ id: '76d65c26-f784-44a2-ac19-586678f7c2f2', name: 'User', email: 'aman.verasia@inventyv.com', password: 'Aman@1234' },
 ];
 
 module.exports = { users };
